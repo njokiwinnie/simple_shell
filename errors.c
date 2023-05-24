22311184fb6b1061f693567f7c806556ed9d92cd
xanderyeng <ckalex54@gmail.com>
2023-05-25 02:55:10 +0300

    SHell test run 28

--- errors.c
+++ errors.c
@@ -1,31 +1,115 @@
-#include "main.h"
+#include "shell.h"
+
+int num_len(int num);
+char *_itoa(int num);
+int create_error(char **args, int err);
 
 /**
- * errors - prints errors based on case
- * @error: error number associated with perror statement
- * Return: void
+ * num_len - Counts the digit length of a number.
+ * @num: The number to measure.
+ *
+ * Return: The digit length.
  */
-void errors(int error)
+int num_len(int num)
 {
-	switch (error)
+	unsigned int num1;
+	int len = 1;
+
+	if (num < 0)
 	{
-	case 1: /* writes fork error to stderr */
-		write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
-		perror("Error");
-		break;
-
-	case 2: /* writes execve error to stderr */
-		perror("Error");
-		break;
-
-	case 3: /* writes malloc error to stderr */
-		write(STDERR_FILENO, ERR_MALLOC, _strlen(ERR_MALLOC));
-		break;
-	case 4: /* writes empty path error to stderr */
-		write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
-		break;
-
-	default:
-		return;
+		len++;
+		num1 = num * -1;
 	}
+	else
+	{
+		num1 = num;
+	}
+	while (num1 > 9)
+	{
+		len++;
+		num1 /= 10;
+	}
+
+	return (len);
+}
+
+/**
+ * _itoa - Converts an integer to a string.
+ * @num: The integer.
+ *
+ * Return: The converted string.
+ */
+char *_itoa(int num)
+{
+	char *buffer;
+	int len = num_len(num);
+	unsigned int num1;
+
+	buffer = malloc(sizeof(char) * (len + 1));
+	if (!buffer)
+		return (NULL);
+
+	buffer[len] = '\0';
+
+	if (num < 0)
+	{
+		num1 = num * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		num1 = num;
+	}
+
+	len--;
+	do {
+		buffer[len] = (num1 % 10) + '0';
+		num1 /= 10;
+		len--;
+	} while (num1 > 0);
+
+	return (buffer);
+}
+
+
+/**
+ * create_error - Writes a custom error message to stderr.
+ * @args: An array of arguments.
+ * @err: The error value.
+ *
+ * Return: The error value.
+ */
+int create_error(char **args, int err)
+{
+	char *error;
+
+	switch (err)
+	{
+	case -1:
+		error = error_env(args);
+		break;
+	case 1:
+		error = error_1(args);
+		break;
+	case 2:
+		if (*(args[0]) == 'e')
+			error = error_2_exit(++args);
+		else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
+			error = error_2_syntax(args);
+		else
+			error = error_2_cd(args);
+		break;
+	case 126:
+		error = error_126(args);
+		break;
+	case 127:
+		error = error_127(args);
+		break;
+	}
+	write(STDERR_FILENO, error, _strlen(error));
+
+	if (error)
+		free(error);
+	return (err);
+
 }
