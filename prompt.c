9f53cfe048b23b6272383629034fb0630165d6b5
xanderyeng <ckalex54@gmail.com>
2023-05-24 16:23:43 +0300

    Checker test No.12

--- prompt.c
+++ prompt.c
@@ -10,5 +10,22 @@
 
 ssize_t get_input(char **input, size_t *n)
 {
-	return (getline(input, n, stdin));
+	ssize_t read = getline(input, n, stdin);
+
+	if (read == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n"); 
+			/* newline after Ctrl+D */
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return read;
 }
