9f20f9750d97aead33eb43aeeb62f6bbfcd42c50
njokiwinnie <winnleykim@gmail.com>
2023-05-26 23:31:02 +0300

    fixed free buffer

--- memoryf.c
+++ memoryf.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * free_all - frees all malloc'd space at end of main loop
- * @tokens: pointer to tokens array
- * @path: pointer to path variable
- * @line: pointer to user input buffer
- * @fullpath: pointer to full path
- * @flag: flag marking if full_path was malloc'd
+ * free_all - frees malloc
+ * @tokens: token array pointer
+ * @path: path variable 
+ * @line: input buffer pointer
+ * @fullpath: path
+ * @flag: flag mark
  * Return: void
  */
 void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
