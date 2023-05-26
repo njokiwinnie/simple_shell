8e757a80163ca58a7f151d8ffcfc30a8e42a5fa4
njokiwinnie <winnleykim@gmail.com>
2023-05-26 23:33:59 +0300

    revert to original for compilation

--- errors.c
+++ errors.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * errors - prints errors based on case
- * @error: error number associated with perror statement
+ * errors - case error
+ * @error: number of error on perror
  * Return: void
  */
 void errors(int error)
 {
 	switch (error)
 	{
-	case 1: /* writes fork error to stderr */
+	case 1: /* fork error to stderr */
 		write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
 		perror("Error");
 		break;
 
-	case 2: /* writes execve error to stderr */
+	case 2: /*  execve error to stderr */
 		perror("Error");
 		break;
 
-	case 3: /* writes malloc error to stderr */
+	case 3: /* malloc error to stderr */
 		write(STDERR_FILENO, ERR_MALLOC, _strlen(ERR_MALLOC));
 		break;
-	case 4: /* writes empty path error to stderr */
+	case 4: /* empty path error to stderr */
 		write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
 		break;
 
